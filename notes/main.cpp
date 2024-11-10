4392bb498cf3e388b12eae41a9065dea35c32a55
nitroglycerine <suichen6@gmail.com>
2024-11-09 23:27:12 -0800

    前进到dense ...
    并且：下标用array会比用vector快很多

--- notes/main.cpp
+++ notes/main.cpp
@@ -12,6 +12,8 @@
 #include <H5Cpp.h>
 #include <sndfile.h>
 
+#include <gtest/gtest.h>
+
 struct MyTensor;
 MyTensor ComputeFBankData(const std::vector<short> wavsignal);
 
@@ -33,7 +35,7 @@ struct MyTensor {
         v.clear();
         v.resize(std::accumulate(d.begin(), d.end(), 1, std::multiplies<int>()));
     }
-    float& at(const std::vector<int>& idx) {
+    float& at(const std::array<int, 4>& idx) {  // Huge speed gain compared to using vector<int> for indices
         int flatidx = 0;
         int mult = 1;
         for (int i=dim.size()-1; i>=0; i--) {
@@ -44,12 +46,12 @@ struct MyTensor {
         return v.at(flatidx);
     }
     void Print() {
-        std::vector<int> idx(dim.size());
+        std::array<int, 4> idx{};
         bool done = false;
         while (!done) {
             printf("%5g ", at(idx));
-            idx.back()++;
-            for (int i=idx.size()-1; i>=0; i--) {
+            idx[dim.size()-1]++;
+            for (int i=dim.size()-1; i>=0; i--) {
                 if (idx.at(i) >= dim.at(i)) {
                     if (i == 0) {
                         done = true;
@@ -98,22 +100,25 @@ struct MyConv2D {
         std::vector<float> out_v(std::accumulate(out_dim.begin(), out_dim.end(), 1, std::multiplies<int>()));
         MyTensor ret(out_v, out_dim);
 
-        // 1 channel input only for now
-        if (in.dim[3] != 1) {
-            assert(0 && "Unimplemented");
-        } 
+        assert(in.dim[3] == kernel.dim[2]);  // Channel
 
+        #pragma omp parallel for
         for (int k=0; k<kernel.dim[3]; k++) {
+            #pragma omp parallel for
             for (int y=0; y<out_dim[1]; y++) {
+                #pragma omp simd
                 for (int x=0; x<out_dim[2]; x++) {
                     float s = 0;
-                    for (int dx = -kernel.dim[1]/2, kx=0; dx <= kernel.dim[1]/2; dx++, kx++) {
-                        for (int dy = -kernel.dim[0]/2, ky=0; dy <= kernel.dim[0]/2; dy++, ky++) {
-                            float in_elt = 0;
-                            if (y+dy >= 0 && x+dx >= 0 && y+dy < in.dim[1] && x+dx < in.dim[2]) {
-                                in_elt = in.at({0, y+dy, x+dx, 0});
+                    #pragma omp simd
+                    for (int z=0; z<kernel.dim[2]; z++) {
+                        for (int dx = -kernel.dim[1]/2, kx=0; dx <= kernel.dim[1]/2; dx++, kx++) {
+                            for (int dy = -kernel.dim[0]/2, ky=0; dy <= kernel.dim[0]/2; dy++, ky++) {
+                                float in_elt = 0;
+                                if (y+dy >= 0 && x+dx >= 0 && y+dy < in.dim[1] && x+dx < in.dim[2]) {
+                                    in_elt = in.at({0, y+dy, x+dx, z});
+                                }
+                                s += kernel.at({ky, kx, z, k}) * in_elt;
                             }
-                            s += kernel.at({ky, kx, 0, k}) * in_elt;
                         }
                     }
                     s += bias.at({k});
@@ -160,17 +165,28 @@ struct MyDense {
     MyTensor bias;
     MyDense(const MyTensor& k, const MyTensor& b) : kernel(k), bias(b) {}
     MyTensor operator()(MyTensor& in) {
-        assert(in.dim.size() == 1);
-        assert(in.dim[0] == kernel.dim[0]);
-        MyTensor out({kernel.dim[1]});
+        assert(in.dim.size() == 1 || in.dim.size() == 2);
+        assert(in.dim.back() == kernel.dim[0]);
+        int len = (in.dim.size()>1) ? in.dim[in.dim.size()-2] : 1;
+        MyTensor out({len, kernel.dim[1]});
         
-        for (int k=0; k<kernel.dim[1]; k++) {
+        for (int i=0; i<len; i++) {
+          for (int k=0; k<kernel.dim[1]; k++) {
             float elt = 0;
-            for (int y=0; y<in.dim[0]; y++) {
+            for (int y=0; y<in.dim.back(); y++) {
+              if (in.dim.size() == 1) {
                 elt += in.at({y}) * kernel.at({y, k});
+              } else {
+                elt += in.at({i,y}) * kernel.at({y, k});
+              }
             }
             elt += bias.at({k});
-            out.at({k}) = elt;
+            out.at({i,k}) = elt;
+          }
+        }
+
+        if (in.dim.size() == 1) {
+          out = out.Reshape({ kernel.dim[1] });
         }
         
         return out;
@@ -182,6 +198,33 @@ struct MyReshape {
     MyReshape(const std::vector<int>& o) : outdim(o) {}
     MyTensor operator()(MyTensor& in) {
         return in.Reshape(outdim);
+    }
+};
+
+struct MyMaxPooling2D {
+    // Assume (batch, width, height, channel)
+    // Assume pool size is (2,2)
+    MyTensor operator()(MyTensor& in) {
+        std::vector<int> d = in.dim;
+        d[1] = (d[1]-2) / 2 + 1;
+        d[2] = (d[2]-2) / 2 + 1;
+        MyTensor ret(d);
+        const int pw = 2, ph = 2;
+        #pragma omp parallel for
+        for (int c=0; c<d[3]; c++) {
+            for (int y=0; y<in.dim[1]; y+=ph) {
+                for (int x=0; x<in.dim[2]; x+=pw) {
+                    float elt = in.at({0,y,x,c});
+                    for (int yy=y; yy<y+ph; yy++) {
+                        for (int xx=x; xx<x+pw; xx++) {
+                            elt = std::max(elt, in.at({0,yy,xx,c}));
+                        }
+                    }
+                    ret.at({0,y/ph,x/pw,c}) = elt;
+                }
+            }
+        }
+        return ret;
     }
 };
 
@@ -211,7 +254,7 @@ MyTensor ReadMyTensorFromH5(const H5::H5File* file, const char* path, int force_
     return MyTensor(data, d);
 }
 
-void MiniTest() {
+TEST(MyDfcnnNotes, MiniTest) {
     try {
         // Step 2: Open the HDF5 file
         H5::H5File file("onelayer.h5", H5F_ACC_RDONLY);
@@ -315,11 +358,12 @@ MyTensor PadAndReshapeFBankData(MyTensor& fbanks) {
     return ret;
 }
 
-void SndfileTest() {
+TEST(MyDfcnnNotes, SndfileTest) {
   // 1. Load sound data
   const char *fn;
   SNDFILE *inFile;
   SF_INFO sfinfo;
+  const float EPS = 1e-3;
 
   fn = "laihongduiquyan.wav";
 
@@ -391,16 +435,259 @@ void SndfileTest() {
   blah = batch_normalization(blah);
   printf("batch_normalization output:\n");
   blah.Summary();
-  printf("%g\n", blah.at({0,0,0,0}));
+//   printf("%g\n", blah.at({0,0,0,0}));  // -0.763677
+//   printf("%g\n", blah.at({0,0,0,1}));  // -0.256286
+//   printf("%g\n", blah.at({0,0,1,0}));  // -0.763677
+//   printf("%g\n", blah.at({0,351,199,31}));  // -0.529448
+
+  printf("conv2d_1:\n");
+  MyConv2D conv2d_1(
+    ReadMyTensorFromH5(&file, "/conv2d_1/conv2d_1/kernel:0"),
+    ReadMyTensorFromH5(&file, "/conv2d_1/conv2d_1/bias:0")
+  );
+  conv2d_1.activation = MyActivationFunc::RELU;
+  blah = conv2d_1(blah);
+  blah.Summary();
+//   printf("%g\n", blah.at({0,0,0,0}));  // 5.84069
+//   printf("%g\n", blah.at({0,0,0,1}));  // 0
+//   printf("%g\n", blah.at({0,0,1,0}));  // 8.37161
+//   printf("%g\n", blah.at({0,351,199,31}));  // 1.90011
+
+  printf("batch_normalization_1:\n");
+  MyBatchNormalization batch_normalization_1(
+    ReadMyTensorFromH5(&file, "/batch_normalization_1/batch_normalization_1/beta:0"),
+    ReadMyTensorFromH5(&file, "/batch_normalization_1/batch_normalization_1/gamma:0"),
+    ReadMyTensorFromH5(&file, "/batch_normalization_1/batch_normalization_1/moving_mean:0"),
+    ReadMyTensorFromH5(&file, "/batch_normalization_1/batch_normalization_1/moving_variance:0")
+  );
+  blah = batch_normalization_1(blah);
+//   printf("%g\n", blah.at({0,0,0,0}));  // 5.84069
+//   printf("%g\n", blah.at({0,0,0,1}));  // -0.39618
+//   printf("%g\n", blah.at({0,0,1,0}));  // 7.95909
+//   printf("%g\n", blah.at({0,351,199,31}));  // 2.11386
+
+  printf("max_pooling2d:\n");
+  MyMaxPooling2D max_pooling2d;
+  blah = max_pooling2d(blah);
+  blah.Summary();
+  printf("%g\n", blah.at({0,0,0,0}));  // 7.95909
+  printf("%g\n", blah.at({0,0,0,1}));  // 23.0257
+  printf("%g\n", blah.at({0,0,1,0}));  // 6.3113
+  printf("%g\n", blah.at({0,175,99,31}));  // 2.54801
+
+  printf("conv2d_2:\n");
+  MyConv2D conv2d_2(
+    ReadMyTensorFromH5(&file, "/conv2d_2/conv2d_2/kernel:0"),
+    ReadMyTensorFromH5(&file, "/conv2d_2/conv2d_2/bias:0")
+  );
+  conv2d_2.activation = MyActivationFunc::RELU;
+  blah = conv2d_2(blah);
+  blah.Summary();
+//   printf("%g\n", blah.at({0,0,0,0}));   // 0
+//   printf("%g\n", blah.at({0,0,0,1}));   // 0
+//   printf("%g\n", blah.at({0,0,0,62}));  // 1.46326959
+//   printf("%g\n", blah.at({0,0,1,0}));   // 0
+//   printf("%g\n", blah.at({0,0,1,2}));   // 8.027619
+//   printf("%g\n", blah.at({0,175,99,62}));  // 0.28268975
+
+  printf("batch_normalization_2:\n");
+  MyBatchNormalization batch_normalization_2(
+    ReadMyTensorFromH5(&file, "/batch_normalization_2/batch_normalization_2/beta:0"),
+    ReadMyTensorFromH5(&file, "/batch_normalization_2/batch_normalization_2/gamma:0"),
+    ReadMyTensorFromH5(&file, "/batch_normalization_2/batch_normalization_2/moving_mean:0"),
+    ReadMyTensorFromH5(&file, "/batch_normalization_2/batch_normalization_2/moving_variance:0")
+  );
+  blah = batch_normalization_2(blah);
+  blah.Summary();
+//   printf("%g\n", blah.at({0,0,0,0}));      // -0.625641
+//   printf("%g\n", blah.at({0,0,0,1}));      // -0.699753
+//   printf("%g\n", blah.at({0,0,1,0}));      // -0.625641
+//   printf("%g\n", blah.at({0,175,99,63}));  // -0.287169
+
+  printf("conv2d_3:\n");
+  MyConv2D conv2d_3(
+    ReadMyTensorFromH5(&file, "/conv2d_3/conv2d_3/kernel:0"),
+    ReadMyTensorFromH5(&file, "/conv2d_3/conv2d_3/bias:0")
+  );
+  conv2d_3.activation = MyActivationFunc::RELU;
+  blah = conv2d_3(blah);
+  printf("%g\n", blah.at({0,0,0,0}));      // 0.495416
+  printf("%g\n", blah.at({0,0,0,1}));      // 3.65815
+  printf("%g\n", blah.at({0,0,1,0}));      // 5.69639
+  printf("%g\n", blah.at({0,175,99,63}));  // 0.874305
+
+  printf("batch_normalization_3:\n");
+  MyBatchNormalization batch_normalization_3(
+    ReadMyTensorFromH5(&file, "/batch_normalization_3/batch_normalization_3/beta:0"),
+    ReadMyTensorFromH5(&file, "/batch_normalization_3/batch_normalization_3/gamma:0"),
+    ReadMyTensorFromH5(&file, "/batch_normalization_3/batch_normalization_3/moving_mean:0"),
+    ReadMyTensorFromH5(&file, "/batch_normalization_3/batch_normalization_3/moving_variance:0")
+  );
+  blah = batch_normalization_3(blah);
+  blah.Summary();
+  printf("%g\n", blah.at({0,0,0,0}));      // -0.116748
+  printf("%g\n", blah.at({0,0,0,1}));      // 3.52502
+  printf("%g\n", blah.at({0,0,1,0}));      // 4.14682
+  printf("%g\n", blah.at({0,175,99,63}));  // 0.227259
+
+  printf("max_pooling2d_1:\n");
+  MyMaxPooling2D max_pooling2d_1;
+  blah = max_pooling2d_1(blah);
+  blah.Summary();
+  printf("%g\n", blah.at({0,0,0,0}));  // 4.146818
+  printf("%g\n", blah.at({0,0,0,1}));  // 9.339903
+  printf("%g\n", blah.at({0,0,1,0}));  // 3.93797
+  printf("%g\n", blah.at({0,87,49,63}));  // 3.08886
+
+  printf("conv2d_4:\n");
+  MyConv2D conv2d_4(
+    ReadMyTensorFromH5(&file, "/conv2d_4/conv2d_4/kernel:0"),
+    ReadMyTensorFromH5(&file, "/conv2d_4/conv2d_4/bias:0")
+  );
+  conv2d_4.activation = MyActivationFunc::RELU;
+  blah = conv2d_4(blah);
+  blah.Summary();
+  printf("%g\n", blah.at({0,0,0,0}));      // 0
+  printf("%g\n", blah.at({0,0,0,1}));      // 0
+  printf("%g\n", blah.at({0,0,1,0}));      // 2.64033
+  printf("%g\n", blah.at({0,87,49,127}));  // 4.47773
+
+  printf("batch_normalization_4:\n");
+  MyBatchNormalization batch_normalization_4(
+    ReadMyTensorFromH5(&file, "/batch_normalization_4/batch_normalization_4/beta:0"),
+    ReadMyTensorFromH5(&file, "/batch_normalization_4/batch_normalization_4/gamma:0"),
+    ReadMyTensorFromH5(&file, "/batch_normalization_4/batch_normalization_4/moving_mean:0"),
+    ReadMyTensorFromH5(&file, "/batch_normalization_4/batch_normalization_4/moving_variance:0")
+  );
+  blah = batch_normalization_4(blah);
+  blah.Summary();
+  printf("%g\n", blah.at({0,0,0,0}));      // -0.811309
+  printf("%g\n", blah.at({0,0,0,1}));      // -0.283161
+  printf("%g\n", blah.at({0,0,1,0}));      // 1.00378
+  printf("%g\n", blah.at({0,87,49,127}));  // 1.51089
+
+  printf("conv2d_5:\n");
+  MyConv2D conv2d_5(
+    ReadMyTensorFromH5(&file, "/conv2d_5/conv2d_5/kernel:0"),
+    ReadMyTensorFromH5(&file, "/conv2d_5/conv2d_5/bias:0")
+  );
+  conv2d_5.activation = MyActivationFunc::RELU;
+  blah = conv2d_5(blah);
+  blah.Summary();
+  printf("%g\n", blah.at({0,0,0,0}));      // 0
+  printf("%g\n", blah.at({0,0,0,1}));      // 4.19674
+  printf("%g\n", blah.at({0,0,1,0}));      // 0
+  printf("%g\n", blah.at({0,87,49,127}));  // 2.98318
+
+  printf("batch_normalization_5:\n");
+  MyBatchNormalization batch_normalization_5(
+    ReadMyTensorFromH5(&file, "/batch_normalization_5/batch_normalization_5/beta:0"),
+    ReadMyTensorFromH5(&file, "/batch_normalization_5/batch_normalization_5/gamma:0"),
+    ReadMyTensorFromH5(&file, "/batch_normalization_5/batch_normalization_5/moving_mean:0"),
+    ReadMyTensorFromH5(&file, "/batch_normalization_5/batch_normalization_5/moving_variance:0")
+  );
+  blah = batch_normalization_5(blah);
+  blah.Summary();
+  printf("%g\n", blah.at({0,0,0,0}));      // -0.744354
+  printf("%g\n", blah.at({0,0,0,1}));      // 2.1443
+  printf("%g\n", blah.at({0,0,1,0}));      // -0.774354
+  printf("%g\n", blah.at({0,87,49,127}));  // 1.74953
+
+  printf("max_pooling_2d_2:\n");
+  MyMaxPooling2D max_pooling2d_2;
+  blah = max_pooling2d_2(blah);
+  blah.Summary();
+  printf("%g\n", blah.at({0,0,0,0}));      // 4.68222
+  printf("%g\n", blah.at({0,0,0,1}));      // 2.78184
+  printf("%g\n", blah.at({0,0,1,0}));      // 4.21254
+  printf("%g\n", blah.at({0,43,24,127}));  // 1.74953
+
+  printf("conv2d_6:\n");
+  MyConv2D conv2d_6(
+    ReadMyTensorFromH5(&file, "/conv2d_6/conv2d_6/kernel:0"),
+    ReadMyTensorFromH5(&file, "/conv2d_6/conv2d_6/bias:0")
+  );
+  conv2d_6.activation = MyActivationFunc::RELU;
+  blah = conv2d_6(blah);
+  blah.Summary();
+  printf("%g\n", blah.at({0,0,0,0}));      // 4.0748
+  printf("%g\n", blah.at({0,0,0,1}));      // 0.936899
+  printf("%g\n", blah.at({0,0,1,0}));      // 3.25749
+  printf("%g\n", blah.at({0,43,24,127}));  // 0
+
+  printf("batch_normalization_6:\n");
+  MyBatchNormalization batch_normalization_6(
+    ReadMyTensorFromH5(&file, "/batch_normalization_6/batch_normalization_6/beta:0"),
+    ReadMyTensorFromH5(&file, "/batch_normalization_6/batch_normalization_6/gamma:0"),
+    ReadMyTensorFromH5(&file, "/batch_normalization_6/batch_normalization_6/moving_mean:0"),
+    ReadMyTensorFromH5(&file, "/batch_normalization_6/batch_normalization_6/moving_variance:0")
+  );
+  blah = batch_normalization_6(blah);
+  blah.Summary();
+  EXPECT_NEAR(blah.at({0,0,0,0}), 2.58825, EPS);
+  EXPECT_NEAR(blah.at({0,0,0,1}), 0.59752, EPS);
+  EXPECT_NEAR(blah.at({0,0,1,0}), 1.99109, EPS);
+  EXPECT_NEAR(blah.at({0,43,24,127}), -0.294892, EPS);  // 0
+
+  printf("conv2d_7:\n");
+  MyConv2D conv2d_7(
+    ReadMyTensorFromH5(&file, "/conv2d_7/conv2d_7/kernel:0"),
+    ReadMyTensorFromH5(&file, "/conv2d_7/conv2d_7/bias:0")
+  );
+  conv2d_7.activation = MyActivationFunc::RELU;
+  blah = conv2d_7(blah);
+  blah.Summary();
+  ASSERT_LE(fabs(blah.at({0,0,0,0}) - 0), EPS);
+  ASSERT_LE(fabs(blah.at({0,0,0,1}) - 3.121731), EPS);
+  ASSERT_LE(fabs(blah.at({0,0,1,0}) - 0), EPS);
+  ASSERT_LE(fabs(blah.at({0,43,24,127}) - (0)), EPS);  // 0
+
+  printf("batch_normalization_7:\n");
+  MyBatchNormalization batch_normalization_7(
+    ReadMyTensorFromH5(&file, "/batch_normalization_7/batch_normalization_7/beta:0"),
+    ReadMyTensorFromH5(&file, "/batch_normalization_7/batch_normalization_7/gamma:0"),
+    ReadMyTensorFromH5(&file, "/batch_normalization_7/batch_normalization_7/moving_mean:0"),
+    ReadMyTensorFromH5(&file, "/batch_normalization_7/batch_normalization_7/moving_variance:0")
+  );
+  blah = batch_normalization_7(blah);
+  blah.Summary();
+  EXPECT_NEAR(blah.at({0,0,0,0}), -0.7206889, EPS);
+  EXPECT_NEAR(blah.at({0,0,0,1}),  1.8955716, EPS);
+  EXPECT_NEAR(blah.at({0,0,1,0}), -0.7206889, EPS);
+  EXPECT_NEAR(blah.at({0,43,24,127}), -0.52239907, EPS);  // 0
+
+  printf("reshape:\n");
+  MyReshape myreshape({blah.dim[1], 3200});
+  blah = myreshape(blah);
+  blah.Summary();
+  EXPECT_NEAR(blah.at({0,0}),    -0.7206889,  EPS);
+  EXPECT_NEAR(blah.at({0,3199}), -0.49066168, EPS);
+  EXPECT_NEAR(blah.at({1,0}),    -0.7206889,  EPS);
+  EXPECT_NEAR(blah.at({1,3199}), -0.38050574, EPS);
+  EXPECT_NEAR(blah.at({43,0}),   -0.7206889, EPS);
+  EXPECT_NEAR(blah.at({43,3199}),-0.52239907, EPS);
+
+  printf("dense:\n");
+  MyDense mydense(
+    ReadMyTensorFromH5(&file, "/dense/dense/kernel:0"),
+    ReadMyTensorFromH5(&file, "/dense/dense/bias:0")
+  );
+  blah = mydense(blah);
+  blah.Summary();
+  EXPECT_NEAR(blah.at({0,0}),   0, EPS);
+  EXPECT_NEAR(blah.at({0,1}),   0.1470249,  EPS);
+  EXPECT_NEAR(blah.at({0,255}), 0.44234842, EPS);
+  EXPECT_NEAR(blah.at({1,255}), 0.8988983,  EPS);
+  EXPECT_NEAR(blah.at({42,1}), 0.19417849,  EPS);
 
   file.close();
   sf_close(inFile);
 }
 
-int main() {
-    H5open();
-    MiniTest();
-    SndfileTest();
-    H5close();
-    return 0;
-}
+// int main() {
+//     H5open();
+//     MiniTest();
+//     SndfileTest();
+//     H5close();
+//     return 0;
+// }
